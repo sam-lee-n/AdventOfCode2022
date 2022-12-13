1b88b3f33393e2147823e82c449b37467e7027a8
Sam Lee <sam.lee@n-able.com>
2022-12-13 22:04:42 +0000

    Day 09 part 2

--- Day09/advent09.py
+++ Day09/advent09.py
@@ -1,3 +1,4 @@
+import math
 with open('input.txt') as f:
     lines = f.readlines()
 
@@ -16,22 +17,54 @@ dirLut = {
     'R': [1,  0]
 }
 
-headLoc = [0, 0]
-tailLoc = [0, 0]
-history = [[0, 0]]
+def cmp(a, b):
+    return (a > b) - (a < b)
+
+
+# headLoc = [0, 0]
+# tailLoc = [0, 0]
+# history = [[0, 0]]
+
+# for line in lines:
+#     dir, steps = line.split()
+#     lastHead = headLoc
+
+#     for _ in range(int(steps)):
+#         lastHead = headLoc
+#         headLoc = [sum(x) for x in zip(headLoc, dirLut[dir])]
+
+#         if tooFar(headLoc, tailLoc):
+#             tailLoc = lastHead
+#         # print(dir, steps, headLoc, tailLoc)
+#         history.append(tailLoc)
+
+# # Part 1
+# print(len(set(tuple(row) for row in history)))
+
+ropeLenght = 10
+rope = [[0, 0]] * ropeLenght
+history1 = [[0, 0]]
+history9 = [[0, 0]]
 
 for line in lines:
     dir, steps = line.split()
-    lastHead = headLoc
 
     for _ in range(int(steps)):
-        lastHead = headLoc
-        headLoc = [sum(x) for x in zip(headLoc, dirLut[dir])]
+        preRope = rope.copy()
+        rope[0] = [sum(x) for x in zip(rope[0], dirLut[dir])]
 
-        if tooFar(headLoc, tailLoc):
-            tailLoc = lastHead
-        # print(dir, steps, headLoc, tailLoc)
-        history.append(tailLoc)
+        for knot in range(1, ropeLenght):
+            hx, hy = rope[knot - 1]
+            tx, ty = rope[knot]
+        
+            if abs(hx - tx) >= 2 or abs(hy - ty) >= 2:
+                tx += cmp(hx, tx)
+                ty += cmp(hy, ty)
+                rope[knot] = [tx, ty]
 
-# Part 1
-print(len(set(tuple(row) for row in history)))
+        history1.append(rope[1])
+        history9.append(rope[-1])
+
+# Part 2
+print(len(set(tuple(row) for row in history1)))
+print(len(set(tuple(row) for row in history9)))
